a0bd3ef2eb4c443dfebe8ffe9516ee229328cb1e
2breadkey <2breadkey@gmail.com>
2020-10-14 20:41:16 +0900

    fix fly button

--- lib/screen/game_screen.dart
+++ lib/screen/game_screen.dart
@@ -34,80 +34,82 @@ class _GameScreenState extends State<GameScreen> {
   }
 
   @override
-  Widget build(BuildContext context) => Container(
-        color: Colors.lightBlue,
-        child: SizedBox(
-            width: 10000,
-            height: FlyingGame.gameHeight * gameRatio,
-            child: Stack(
-              children: [
-                Align(
-                  key: ValueKey("field"),
-                  alignment: Alignment.bottomLeft,
-                  child: Transform.translate(
-                      offset: Offset(
-                          (-game.flyer.x + game.flyer.spriteWidth / 2) *
-                              gameRatio,
-                          0),
-                      child: Stack(alignment: Alignment.bottomLeft, children: [
-                        Stack(
-                            children: game.fields
-                                .map((field) => SizedBox(
-                                      key: ValueKey(field),
-                                      width: field.width * gameRatio,
-                                      child: Stack(
-                                        alignment: Alignment.bottomLeft,
-                                        children: field.walls
-                                            .expand((e) => e)
-                                            .map((wall) => Transform.translate(
-                                                  key: ValueKey(wall),
-                                                  offset: center(wall),
-                                                  child: SizedBox(
-                                                    width: wall.spriteWidth *
-                                                        gameRatio,
-                                                    height: wall.spriteHeight *
-                                                        gameRatio,
-                                                    child: wall.sprite,
-                                                  ),
-                                                ))
-                                            .toList(),
-                                      ),
-                                    ))
-                                .toList()),
-                        Transform.translate(
-                            key: ValueKey(game.flyer),
-                            offset: center(game.flyer),
-                            child: Transform.rotate(
-                              angle: game.flyer.angle,
-                              child: SizedBox(
-                                  width: game.flyer.spriteWidth * gameRatio,
-                                  height: game.flyer.spriteHeight * gameRatio,
-                                  child: game.flyer.sprite),
-                            )),
-                      ])),
-                ),
-                Align(
+  Widget build(BuildContext context) => Scaffold(
+        body: Container(
+          color: Colors.lightBlue,
+          child: SizedBox(
+              width: 10000,
+              height: FlyingGame.gameHeight * gameRatio,
+              child: Stack(
+                children: [
+                  Align(
+                    key: ValueKey("field"),
                     alignment: Alignment.bottomLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.all(24),
-                      child: MaterialButton(
-                        color: Colors.pink,
-                        onHighlightChanged: (value) {
-                          if (value) {
-                            game.startFly();
-                          } else {
-                            game.endFly();
-                          }
-                        },
-                        onPressed: () {
-                          if (game.isGameOver) {
-                            game.startGame();
-                          }
-                        },
-                      ),
-                    ))
-              ],
-            )),
+                    child: Transform.translate(
+                        offset: Offset(
+                            (-game.flyer.x + game.flyer.spriteWidth / 2) *
+                                gameRatio,
+                            0),
+                        child:
+                            Stack(alignment: Alignment.bottomLeft, children: [
+                          Stack(
+                              children: game.fields
+                                  .map((field) => SizedBox(
+                                        key: ValueKey(field),
+                                        width: field.width * gameRatio,
+                                        child: Stack(
+                                          alignment: Alignment.bottomLeft,
+                                          children: field.walls
+                                              .expand((e) => e)
+                                              .map(
+                                                  (wall) => Transform.translate(
+                                                        key: ValueKey(wall),
+                                                        offset: center(wall),
+                                                        child: SizedBox(
+                                                          width:
+                                                              wall.spriteWidth *
+                                                                  gameRatio,
+                                                          height:
+                                                              wall.spriteHeight *
+                                                                  gameRatio,
+                                                          child: wall.sprite,
+                                                        ),
+                                                      ))
+                                              .toList(),
+                                        ),
+                                      ))
+                                  .toList()),
+                          Transform.translate(
+                              key: ValueKey(game.flyer),
+                              offset: center(game.flyer),
+                              child: Transform.rotate(
+                                angle: game.flyer.angle,
+                                child: SizedBox(
+                                    width: game.flyer.spriteWidth * gameRatio,
+                                    height: game.flyer.spriteHeight * gameRatio,
+                                    child: game.flyer.sprite),
+                              )),
+                        ])),
+                  ),
+                ],
+              )),
+        ),
+        floatingActionButton: MaterialButton(
+          child: const Icon(Icons.flight_takeoff, color: Colors.white),
+          color: Colors.pink,
+          onPressed: () {},
+          onHighlightChanged: (value) {
+            if (value) {
+              if (game.isGameOver) {
+                game.startGame();
+              } else {
+                game.startFly();
+              }
+            } else {
+              game.endFly();
+            }
+          },
+        ),
       );
 
   Offset center(GameObject gameObject) =>

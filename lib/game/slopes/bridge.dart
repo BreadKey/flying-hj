396ca260bae8fe76d6deecedffbc5558414c1fb0
2breadkey <2breadkey@gmail.com>
2020-10-22 00:27:34 +0900

    fix building under bridge

--- lib/game/slopes/bridge.dart
+++ lib/game/slopes/bridge.dart
@@ -41,7 +41,7 @@ class _BridgePainter extends CustomPainter {
     if (size.height > size.width * 4) {
       paint.style = PaintingStyle.fill;
       canvas.drawRect(
-          Rect.fromLTWH(0, size.width * 2, size.width, size.height), paint);
+          Rect.fromLTWH(0, size.width * 2, size.width + 0.1, size.height), paint);
     }
   }
 
